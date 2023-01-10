fc19b7fd584a96dea2bc9c84e6860b2ef8aeb903
Oleydi <oleydi2@gmail.com>
2023-01-10 16:46:08 -0400

    Node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,8 +1,28 @@
 const process = require("process");
-const commands = require("./commands/index.js");
+const commands = require("./commands/index");
+
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+
+}
 
 function bash() {
+  process.stdout.write("prompt > ")
+  process.stdin.on("data", (data) => { 
+    let args = data.toString().trim().split(" ");
+    let cmd = args.shift();
+
+    if (commands[cmd]) {
+      commands[cmd](print, args.join(" "));
+    } else {
+      print(`command not found: ${cmd}`)
+    }
+
+   });
 }
+
+
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,54 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
 
-function cat() {}
-
-function head() {}
-
-function tail() {}
-
-function curl() {}
-
-module.exports = {};
+function ls(print) {
+    fs.readdir(".", function (error, files) {
+      if (error) throw new Error(error);
+      print(files.join(" "));
+    });
+  }
+  
+  function cat(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+      if (error) throw new Error(error);
+      print(data);
+    }); 
+  }
+  
+  function head(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+      if (error) throw new Error(error);
+      const lines = data.split("\n")
+      print(lines[0].trim());
+    });
+  }
+  
+  function tail(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+      if (error) throw new Error(error);
+      const lines = data.split("\n")
+      print(lines[lines.length-1].trim());
+    });
+  }
+  
+  function curl(print, args) {
+      utils.request(args, (error,response)=>{
+          if (error) throw new Error(error);
+          print(response)
+      })
+  }
+  
+  module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
